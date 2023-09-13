e420eab7468c6f24471f17db2779e995f28f6e14
Arthur Lima <Arthurlmsnts@gmail.com>
2023-09-12 22:43:10 -0300

    7. Agrupe os alunos por curso e conte quantos alunos estão matriculados em cada curso.

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,4 +1,3 @@
-SELECT autores.nome, COUNT(livros.id) AS total_de_livros
-FROM autores
-LEFT JOIN livros ON autores.id = livros.autor_id
-GROUP BY autores.nome;
+SELECT curso, COUNT(aluno_id) AS total_de_alunos
+FROM matriculas
+GROUP BY curso;
